ec2dccb04113284368d419e062f3318ec9bccbea
Gilgaladea <gabrysiawolynko@gmail.com>
2026-09-14 21:32:29 +0200

    Procfile i /health

--- app.py
+++ app.py
@@ -745,5 +745,12 @@ def pobierz_podsumowanie(nazwa_pliku):
     )
 
 
+@app.route("/health")
+def health_check():
+    return "OK", 200
+
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    tryb_debug = os.environ.get("FLASK_DEBUG", "True") == "True"
+    app.run(host="0.0.0.0", port=port, debug=tryb_debug)
